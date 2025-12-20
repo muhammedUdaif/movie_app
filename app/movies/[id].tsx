4d5f35634b39fed2216563fbfe858f578f735e97
muhammed-udaif <muhammed.udaif@vonnue.com>
2025-12-20 14:10:52 +0530

    fix: resolve type error in MovieInformation component

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -59,8 +59,8 @@ const MovieDetails = () => {
           <MovieInfo label='Overview' value={movie?.overview}/>
           <MovieInfo  label='Genres' value={movie?.genres?.map((g) => g.name).join("-") || "N/A"}/>
           <View className='flex flex-row justify-between w-1/2'>
-            <MovieInfo label='Budget' value={`$${movie?.budget / 1_000_000} million`}/>
-            <MovieInfo  label='Revenue' value={`${Math.round(movie?.revenue) / 1_000_000}`}/>
+            <MovieInfo label='Budget' value={`$${(movie?.budget) ?? 0 / 1_000_000} million`}/>
+            <MovieInfo  label='Revenue' value={`${Math.round((movie?.revenue ?? 0)) / 1_000_000}`}/>
           </View>
 
           <MovieInfo label='Production Companies' value={movie?.production_companies.map((c) => c.name).join("-") || "N/A"}/>
